f7d147d082d14da8c37570bcffb37c125cec9be7
hongfai <39641383+hongfai@users.noreply.github.com>
2025-12-10 15:09:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "abcde123"
+customdb = "employee"
+custombucket = "semhongfai-employee"
+customregion = "us-east-1"
+
